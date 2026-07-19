8346da256e5f60760dcb6b5e2aede627b1ba9994
deploy <deploy@local>
2026-07-18 21:09:00 -0400

    Deploy static build

---
+++ _next/static/QUOcz69lWiOktDakVNo9k/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/rr-remodel-and-repair//_next/:path+",
+        "destination": "/rr-remodel-and-repair/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
